279ad9c1580f9a9c4c66d28ef180ec3095da82e1
Cai <13110818005@qq.com>
2025-08-30 17:17:22 +0800

    feat: send partial configuration in /bot-ping

--- src/plugins/github/commands.py
+++ src/plugins/github/commands.py
@@ -1,8 +1,11 @@
-from nonebot import on_command
+from nonebot import on_command, get_plugin_config
+from .config import Config
 
 ping = on_command("bot-ping", force_whitespace=True, block=True)
 
-
+config = get_plugin_config(Config)
 @ping.handle()
 async def _():
-    await ping.finish("pong")
+    await ping.finish("pong\n"
+                      f"app_repo: {config.app_repo}\n"
+                      f"test_group_id: {config.test_group_id}")
