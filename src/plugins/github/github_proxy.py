4c667e2ba3d395f661213a0921d54ac93749385a
Cai <13110818005@qq.com>
2025-09-04 22:43:22 +0800

    feat(github): switch to other GitHub proxy on downloading failure

---
+++ src/plugins/github/github_proxy.py
@@ -0,0 +1,30 @@
+import aiohttp
+
+from ... import TEMP_DIR_PATH
+
+PROXYS = [
+    "https://ghproxy.net1/",
+    "https://gh.llkk.cc1/",
+    "https://gh-proxy.com/"
+]
+
+
+class GitHubProxy:
+    current_proxy_index: int = 0
+
+    @classmethod
+    async def download_file(cls, url: str, file_name: str, use_proxy: bool = True):
+        try:
+            if use_proxy:
+                url = PROXYS[cls.current_proxy_index] + url
+
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as response:
+                    file_path = TEMP_DIR_PATH / file_name
+
+                    with open(file_path, 'wb') as f:
+                        async for chunk in response.content.iter_chunked(1024):
+                            f.write(chunk)
+        except:
+            cls.current_proxy_index = (cls.current_proxy_index + 1) % len(PROXYS)  # 环形队列
+            raise
