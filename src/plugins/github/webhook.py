0c4fd6b077b25fc2520ed5fc8dee699218ecba99
Cai <13110818005@qq.com>
2025-12-03 19:08:20 +0800

    fix(release): remove draft condition to ensure asset delivery (#11)
    closed: #9

--- src/plugins/github/webhook.py
+++ src/plugins/github/webhook.py
@@ -41,8 +41,7 @@ async def _(request: Request):
             nonebot.logger.info(f"收到release事件({action}): {release.model_dump_json()}")
             if (repo.full_name == config.app_repo and
                     action == "edited" and  # 只有edit才能拿到assets疑似工作流Bug
-                    release.tag_name == "alpha" and
-                    not release.draft):
+                    release.tag_name == "alpha"):
 
                 git_log = format_git_log(release.body)
 
