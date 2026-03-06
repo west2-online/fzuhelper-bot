5cdd438a4f2a9b41b440a9c42e01f0f9caff2716
Cai <13110818005@qq.com>
2026-03-06 11:56:07 +0800

    feat: add download release test command

--- src/plugins/github/commands.py
+++ src/plugins/github/commands.py
@@ -1,5 +1,10 @@
+import aiohttp
+
 from nonebot import on_command, get_plugin_config
+from nonebot.adapters.onebot.v11 import GroupMessageEvent
 from .config import Config
+from .github_proxy import GitHubProxy
+from .utils import upload_group_file
 
 ping = on_command("bot-ping", force_whitespace=True, block=True)
 
@@ -11,3 +16,22 @@ async def _():
     await ping.finish("pong\n"
                       f"app_repo: {config.app_repo}\n"
                       f"test_group_id: {config.test_group_id}")
+
+
+download_test = on_command("bot-download", force_whitespace=True, block=True)
+
+
+@download_test.handle()
+async def _(event: GroupMessageEvent):
+    api_url = f"https://api.github.com/repos/west2-online/fzuhelper-app/releases/tags/alpha"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(api_url, headers={"Accept": "application/vnd.github+json"}) as resp:
+            resp.raise_for_status()
+            release = await resp.json()
+
+    asset = release["assets"][0]
+    download_url: str = asset["browser_download_url"]
+    file_name: str = asset["name"].replace(".apk", ".Apk")
+
+    await GitHubProxy.download_file(download_url, file_name, True)
+    await upload_group_file(event.group_id, file_name)
