3bfb05efb54b211665cb94224c830abc4f3a3352
ACaiCat <13110818005@qq.com>
2026-09-11 16:22:42 +0800

    fix: 测试通过提示添加标点

--- fzuhelper-bot/review_react.py
+++ fzuhelper-bot/review_react.py
@@ -42,12 +42,12 @@ async def react_msg(
 
     if react_type == "huawei":
         message = (
-            MessageSegment.markdown(f"⭕ Harmony（{version}）已通过 AppTest 测试审核")
+            MessageSegment.markdown(f"⭕ Harmony（{version}）已通过 AppTest 测试审核！")
             + app_test_keyboard
         )
     else:
         message = (
-            MessageSegment.markdown(f"🍎 iOS（{version}）已通过 TestFlight 测试审核")
+            MessageSegment.markdown(f"🍎 iOS（{version}）已通过 TestFlight 测试审核！")
             + test_flight_keyboard
         )
 
